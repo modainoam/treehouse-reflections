f4a7d84880a6c6abbf8a9b9646a765dd921a97a6
Noam <modainoa.m@gmail.com>
2016-12-23 10:26:49 +0200

    Remove Treehouse course folders

--- Course Folders/jqueryplugins-treehouse/animsition/js/main.js
+++
@@ -1,4 +0,0 @@
-$(".animsition").animsition({
-    inClass: 'fade-in-down-sm',
-    outClass: 'fade-out-down-sm',
-    linkElement: 'header a'});
--- Course Folders/js-treehouse/libs.js
+++
@@ -1,2 +0,0 @@
-num2 = parseInt(prompt("Random - Up to which number?"))
-document.write(Math.floor(Math.random() * num2) + 1)
